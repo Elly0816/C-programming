6f6bcc6a619550205fcda87245b16fb71bb892a9
Eleazar Udo <elzoremmanuel@gmail.com>
2025-02-04 20:04:16 +0100

    exponential search

--- C-programming/SearchingAndSortingAlgorithms_1/searchingAlgorithms.c
+++ C-programming/SearchingAndSortingAlgorithms_1/searchingAlgorithms.c
@@ -17,10 +17,12 @@ int linear_search(int items[], int size, int item)
     {
         if (items[i] == item)
         {
+
             printf("Item: %d found at index: %d", item, i);
-            return 1;
+            return 0;
         }
     }
+
     printf("Item not found in the input array");
     return -1;
 }
@@ -37,8 +39,9 @@ int binary_search(int items[], int size, int item)
         mid = left + (right - left) / 2;
         if (items[mid] == item)
         {
-            printf("The item: %d was found at index: %d using binary search", item, mid);
-            return 1;
+
+            printf("The item: %d was found at index: %d using binary search\n", item, mid);
+            return 0;
         }
         else if (items[mid] > item)
         {
@@ -49,14 +52,40 @@ int binary_search(int items[], int size, int item)
             left = mid + 1;
         }
     }
-    printf("Item: %d is not in the array using binary search", item);
+
+    printf("Item: %d is not in the array using binary search\n", item);
 
     return -1;
 }
 
+// Exponential search
+
+int exponential_search(int items[], int size, int item)
+{
+    int lower_bound = 0, upper_bound;
+    if (items[lower_bound] == item)
+    {
+        printf("Item: %d has been found at index: %d", item, lower_bound);
+        return 1;
+    }
+
+    upper_bound = 1;
+    while (upper_bound < size && items[upper_bound] <= item)
+    {
+        upper_bound = upper_bound * 2;
+    }
+    lower_bound = upper_bound / 2;
+
+    upper_bound = (upper_bound > size) ? size - 1 : upper_bound;
+
+    return binary_search(items + lower_bound, (upper_bound - lower_bound) + 1, item);
+}
+
+//
+
 int main()
 {
-    const int SIZE = 2000;
+    const int SIZE = 20000000;
     int *items = (int *)malloc(SIZE * sizeof(int));
 
     if (items == NULL)
@@ -70,13 +99,15 @@ int main()
         items[i] = 10 * (i + 1);
     }
 
-    linear_search(items, SIZE, 19040);
+    linear_search(items, SIZE, 190400500);
 
     qsort(items, SIZE, sizeof(int), compare_int);
 
     printf("\n");
-    binary_search(items, SIZE, 19040);
+    binary_search(items, SIZE, 190400500);
 
+    printf("\n");
+    exponential_search(items, SIZE, 190400500);
     free(items);
     return 0;
 }
