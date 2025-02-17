52db7a2a62959d37702f087145c673457ebdd227
Eleazar Udo <elzoremmanuel@gmail.com>
2025-02-17 08:57:58 +0100

    codecademy c tutorial

---
+++ C-programming/codecademy/tutorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char characterName[] = "John";
+    int characterAge = 35;
+
+    printf("There once was a man named %s.\nHe was %d years old.\nHe liked the name %s.\nBut he didn't like being %d", characterName, characterAge, characterName, characterAge);
+    return 0;
+}
