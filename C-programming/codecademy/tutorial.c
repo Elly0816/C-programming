66e35c9f8bcab55e66705c1b7c50284cd98d507a
Eleazar Udo <elzoremmanuel@gmail.com>
2025-02-22 01:40:23 +0100

    floating point operations

--- C-programming/codecademy/tutorial.c
+++ C-programming/codecademy/tutorial.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    char character = 'F';
-    char characterName[] = "John";
-    int characterAge = 35;
-    float gpa = 3.7;
+    // char character = 'F';
+    // char characterName[20] = "John";
+    // int characterAge = 35;
+    // float gpa = 3.7;
 
-    printf("There once was a man named %s.\nHe was %d years old.\nHe liked the name %s.\nBut he didn't like being %d", characterName, characterAge, characterName, characterAge);
-    printf("\n%c", character);
+    // printf("There was a man named %s", characterName);
+    // printf("\nHe was %d years old", characterAge);
+    // strcpy(characterName, "James");
+    // printf("\nHe really liked the name %s", characterName);
+    // characterAge = 67;
+    // printf("\nBut he did not like being %d", characterAge);
+    // printf("\n%c", character);
+
+    float number = (float)5 / (float)4;
+    printf("%f", number);
     return 0;
 }
