b9e199c2a4850653889e351a4f3a8285e52eb2ff
Eleazar Udo <elzoremmanuel@gmail.com>
2025-03-11 10:00:19 +0100

    optimized pointers

---
+++ C-programming/homework/march_10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char myText[300];
+
+    char *myText_ptr = &myText[0];
+    int length = sizeof(myText) / sizeof(char);
+
+    printf("Enter your text: ");
+    scanf("%[^\n]", myText);
+
+    // for (int i = 0; i < length; i++)
+    // {
+    //     if (*(myText_ptr + i) == '\0')
+    //     {
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         printf("\n%c is stored in: %p", *(myText_ptr + i), myText_ptr + i);
+    //     }
+    // }
+
+    while (*myText_ptr)
+    {
+
+        printf("\n%c is stored in: %p", *(myText_ptr), myText_ptr);
+        *myText_ptr++;
+    }
+    return 0;
+}
